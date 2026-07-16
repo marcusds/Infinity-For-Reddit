522a8686235914c940680dc7bf490aa62c184b8c
marcusds <git@marcusds.ca>
2026-07-15 20:05:35 -0700

    fix: heal missing reminders table for devices stuck on schema v34
    Devices that reached fork schema version 33 via this fork's old
    MIGRATION_31_32 (bookmarks) never ran upstream's real MIGRATION_32_33,
    which creates the reminders table. MIGRATION_33_34 committed anyway,
    leaving these devices at version 34 with no reminders table and a
    permanent crash on startup. Add MIGRATION_34_35 to create the table
    unconditionally for anyone stuck at v34.

--- app/src/main/java/ml/docilealligator/infinityforreddit/RedditDataRoomDatabase.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/RedditDataRoomDatabase.java
@@ -56,7 +56,7 @@ import ml.docilealligator.infinityforreddit.user.UserData;
 @Database(entities = {Account.class, SubredditData.class, SubscribedSubredditData.class, UserData.class,
         SubscribedUserData.class, MultiReddit.class, CustomTheme.class, RecentSearchQuery.class,
         ReadPost.class, PostFilter.class, PostFilterUsage.class, AnonymousMultiredditSubreddit.class,
-        CommentFilter.class, CommentFilterUsage.class, CommentDraft.class, Reminder.class, BookmarkedSubredditData.class}, version = 34, exportSchema = false)
+        CommentFilter.class, CommentFilterUsage.class, CommentDraft.class, Reminder.class, BookmarkedSubredditData.class}, version = 35, exportSchema = false)
 @TypeConverters(Converters.class)
 public abstract class RedditDataRoomDatabase extends RoomDatabase {
 
@@ -70,7 +70,8 @@ public abstract class RedditDataRoomDatabase extends RoomDatabase {
                         MIGRATION_17_18, MIGRATION_18_19, MIGRATION_19_20, MIGRATION_20_21,
                         MIGRATION_21_22, MIGRATION_22_23, MIGRATION_23_24, MIGRATION_24_25,
                         MIGRATION_25_26, MIGRATION_26_27, MIGRATION_27_28, MIGRATION_28_29,
-                        MIGRATION_29_30, MIGRATION_30_31, MIGRATION_31_32, MIGRATION_32_33, ForkMigrations.MIGRATION_33_34)
+                        MIGRATION_29_30, MIGRATION_30_31, MIGRATION_31_32, MIGRATION_32_33, ForkMigrations.MIGRATION_33_34,
+                        ForkMigrations.MIGRATION_34_35)
                 .build();
     }
 
